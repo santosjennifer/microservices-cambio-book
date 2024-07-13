3cca368b622d36c6d0b92c7ffa24a40c8c036b34
santosjennifer <asantos.jennifer@gmail.com>
2024-07-13 15:39:27 -0300

    Implementado resilience4j

--- ms-book/src/main/java/com/jads/service/impl/BookServiceImpl.java
+++ ms-book/src/main/java/com/jads/service/impl/BookServiceImpl.java
@@ -1,5 +1,8 @@
 package com.jads.service.impl;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -12,6 +15,8 @@ import com.jads.exception.BookNotFoundException;
 import com.jads.model.Book;
 import com.jads.repository.BookRepository;
 import com.jads.service.BookService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -28,6 +33,7 @@ public class BookServiceImpl implements BookService {
 	@Autowired
 	private ModelMapper modelMapper;
 
+	@CircuitBreaker(name = "cambioService", fallbackMethod = "fallbackCalculatePrice")
 	public BookDto calculatePrice(String id, String currency) {
 		try {
 			Long bookId = Long.parseLong(id);
@@ -55,5 +61,22 @@ public class BookServiceImpl implements BookService {
 		book.setEnvironment("Book port " + port + " Cambio port " + cambio.getEnvironment());
 		book.setPrice(cambio.getConvertedValue());
 	}
+	
+    public BookDto fallbackCalculatePrice(String id, String currency, Throwable t) {
+        Long bookId = Long.parseLong(id);
+        Book book = repository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(id));
+        
+        BookDto bookDto = modelMapper.map(book, BookDto.class);
+
+        double fixedConversionRate = 5.1;
+        double price = book.getPrice() * fixedConversionRate;
+        BigDecimal formattedPrice = new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
+        
+        bookDto.setPrice(formattedPrice.doubleValue());
+        bookDto.setEnvironment("Fallback: valor fixo utilizado");
+
+        return bookDto;
+    }
 
 }
